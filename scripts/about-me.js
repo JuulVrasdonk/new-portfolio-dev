ff0924b47eb2727e841fb7c534026394dfa3ba06
JuulVrasdonk <56347630+JuulVrasdonk@users.noreply.github.com>
2021-12-19 20:53:38 +0100

    About me image fix

--- scripts/about-me.js
+++ scripts/about-me.js
@@ -1,7 +1,6 @@
 // ************* All imports for index.html ************* //
 import customCursor  from './modules/custom-cursor.js';
 import clickableCursor  from './modules/clickable-cursor.js';
-import cursorText  from './modules/cursor-text.js';
 import magneticBtn from './modules/magnetic-btn.js';
 import pageTransition from './modules/page-transition.js';
 
@@ -12,7 +11,6 @@ import pageTransition from './modules/page-transition.js';
 // ************* Calling the imports ************* //
 customCursor();
 clickableCursor();
-cursorText(); 
 magneticBtn();
 pageTransition();
 
